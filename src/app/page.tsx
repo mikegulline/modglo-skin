b7619138681052aa8aa1f61737e2dedeb499463e
Mike Gulline <mike@mikegulline.com>
2023-11-16 12:04:39 -0800

    updated logo size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className='h-screen flex justify-center items-center'>
-      <Image src={logo} width={200} height={200} alt='ModGlo Skincare Logo' />
+      <Image src={logo} width={300} height={300} alt='ModGlo Skincare Logo' />
     </div>
   );
 }
