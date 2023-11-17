967bf18faf3d8b7a30641fbc85d26d3b1a61d6bc
Mike Gulline <mike@mikegulline.com>
2023-11-17 07:42:26 -0800

    add priority to logo img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,14 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className='h-screen flex justify-center items-center'>
-      <Image src={logo} width={300} height={300} alt='ModGlo Skincare Logo' />
+    <div className='h-screen flex justify-center items-center bg-slate-100'>
+      <Image
+        src={logo}
+        width={300}
+        height={300}
+        alt='ModGlo Skincare Logo'
+        priority={true}
+      />
     </div>
   );
 }
